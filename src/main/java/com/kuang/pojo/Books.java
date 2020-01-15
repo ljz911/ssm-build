9561983cfbb9ddc34e744b8f6e08336d3cb1d667
刘敬祯 <1727033443@qq.com>
2020-01-15 15:56:35 +0800

    Initial commit

---
+++ src/main/java/com/kuang/pojo/Books.java
@@ -0,0 +1,67 @@
+package com.kuang.pojo;
+
+/**
+ * @Title Books
+ * @Description ${}
+ * @Author 刘敬祯
+ * @CreateTime 2020/1/15 13:54
+ * @Version 1.0
+ **/
+public class Books {
+    private int bookID;
+    private String bookName;
+    private int bookCounts;
+    private String detail;
+
+    public Books() {
+    }
+
+    public Books(int bookID, String bookName, int bookCounts, String detail) {
+        this.bookID = bookID;
+        this.bookName = bookName;
+        this.bookCounts = bookCounts;
+        this.detail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return "Books{" +
+                "bookID=" + bookID +
+                ", bookName='" + bookName + '\'' +
+                ", bookCounts=" + bookCounts +
+                ", detail='" + detail + '\'' +
+                '}';
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getBookCounts() {
+        return bookCounts;
+    }
+
+    public void setBookCounts(int bookCounts) {
+        this.bookCounts = bookCounts;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
---
+++ src/main/java/com/kuang/service/BookServiceImpl.java
@@ -0,0 +1,47 @@
+package com.kuang.service;
+
+import com.kuang.dao.BookMapper;
+import com.kuang.pojo.Books;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Title BookServiceImpl
+ * @Description ${}
+ * @Author 刘敬祯
+ * @CreateTime 2020/1/15 14:13
+ * @Version 1.0
+ **/
+
+public class BookServiceImpl implements BookService {
+
+
+    //调用dao层的操作，设置一个set接口，方便Spring管理
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
